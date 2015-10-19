d89ab97442733688f9a0a8fb83c491cf856abd83
lhwhatever <llinhong.2301+github@gmail.com>
2015-10-19 08:30:24 +0000

    Fix

--- acc/models.py
+++ acc/models.py
@@ -14,13 +14,9 @@ class UserProfile(models.Model):
 
     @classmethod
     def get(cls, user):
-<<<<<<< HEAD
-        return cls.objects.get(user=user) if user.is_authenticated() else None
-=======
         return cls.objects.all.get(user=user) if user.is_authenticated() else None
 
 
 class BasePrivacySettings(models.Model):
     owner = models.OneToOneField(User)
     settings = models.CharField(max_length=2, choices=())
->>>>>>> 93d751b32678b643dc2bf2f0d473c6618a2dd0ef
