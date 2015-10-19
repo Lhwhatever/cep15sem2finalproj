6baa8aadfd761e21528c02e15a40ef46081e2da8
lhwhatever <llinhong.2301+github@gmail.com>
2015-10-19 08:39:51 +0000

    Fixed issues with starting game

--- onevone/models.py
+++ onevone/models.py
@@ -5,6 +5,39 @@ from django.db import models
 import acc
 
 
+class Location(models.Model):
+    name = models.CharField(max_length=31)
+    full_text = models.TextField()
+    is_online = models.BooleanField()
+    
+    
+class PrivacySettings(acc.models.BasePrivacySettings):
+    SETTINGS = (
+        (0, 'Completely private'),
+        (1, 'Open for spectating'),
+        (2, 'Open for participation')
+    )
+
+    allow_regardless = models.ForeignKey(acc.models.UserProfile, related_name="allowed_for")
+    restrict_regardless = models.ForeignKey(acc.models.UserProfile, related_name="restricted_for")
+
+    mode = models.IntegerField(verbose_name='Settings', choices=SETTINGS)
+    
+    
+class Match(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    owner = models.OneToOneField(acc.models.UserProfile, related_name="match_owner")
+    participants = models.ForeignKey(acc.models.UserProfile, related_name="match_participants")
+
+    location = models.OneToOneField(Location)
+    time_start = models.DateTimeField(verbose_name="Starting time")
+    time_end = models.DateTimeField(verbose_name="Ending time")
+
+    privacy = models.OneToOneField(PrivacySettings)
+    
+    
 class GameCategory(models.Model):
     name = models.CharField(max_length=31)
     description = models.TextField()
@@ -15,45 +48,12 @@ class GameCategory(models.Model):
         return self.name
 
 
-class Location(models.Model):
-    name = models.CharField(max_length=31)
-    full_text = models.TextField()
-    is_online = models.BooleanField()
-
-
-class PrivacySettings(acc.models.BasePrivacySettings):
-    SETTINGS = (
-        (0, 'Completely private'),
-        (1, 'Open for spectating'),
-        (2, 'Open for participation')
-    )
-
-    allow_regardless = models.ForeignKey(acc.models.UserProfile)
-    restrict_regardless = models.ForeignKey(acc.models.UserProfile)
-
-    settings = models.IntegerField(verbose_name='Settings', choices=SETTINGS)
-
-
-class Match(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-
-    owner = models.OneToOneField(acc.models.UserProfile)
-    participants = models.ForeignKey(acc.models.UserProfile)
-
-    location = models.OneToOneField(Location)
-    time_start = models.DateTimeField(verbose_name="Starting time")
-    time_end = models.DateTimeField(verbose_name="Ending time")
-
-    privacy = models.OneToOneField(PrivacySettings)
-
-
 class Tournament(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
 
-    owner = models.OneToOneField(acc.models.UserProfile)
-    participants = models.ForeignKey(acc.models.UserProfile)
+    owner = models.OneToOneField(acc.models.UserProfile, related_name="tourney_owner")
+    participants = models.ForeignKey(acc.models.UserProfile, related_name="tourney_participants")
 
     location = models.OneToOneField(Location)
     time_start = models.DateTimeField(verbose_name="Starting time")
