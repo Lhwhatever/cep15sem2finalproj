34689af96407689faf30aac986cd8f4ff6b59d2f
lhwhatever <llinhong.2301+github@gmail.com>
2015-10-21 16:29:41 +0000

    Improved filtering functionality

--- onevone/urls.py
+++ onevone/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns(
 
     url(r'^match/l*$', views.MatchListView.as_view(), name='match.index'),
     #url(r'^match/d/(?P<hashcode>[a-zA-Z0-9+/])$', views.MatchDetailView.as_view()),
+    url(r'^match/l/(?P<filter>\w+)', views.MatchListView.as_view(), name='match.filtered'),
     url(r'^match/d/(?P<pk>\d+)', views.MatchDetailView.as_view(), name='match.detail'),
 
     url(r'^event/$', views.EventView.as_view()),
