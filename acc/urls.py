0ae1524d2608b14a235f0b7a95d73db0ce24a01f
lhwhatever <llinhong.2301+github@gmail.com>
2015-10-21 18:46:16 +0000

    a

--- acc/urls.py
+++ acc/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = patterns(
     '',
     url(r'^login/$', views.LoginView.as_view(), name='login'),
+    url(r'^register/$', views.RegisterView.as_view(), name='register'),
+    url(r'^logout/$', views.LogoutView.as_view(), name='logout')
 )
